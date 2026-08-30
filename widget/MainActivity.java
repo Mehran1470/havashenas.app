f118d68ee9e71393b8d8cd6b5cba01b519907bfe
Mehran1470 <mhranzary40@gmail.com>
2026-08-30 07:12:55 +0330

    Update MainActivity.java

--- widget/MainActivity.java
+++ widget/MainActivity.java
@@ -1,5 +1,7 @@
 package com.havashenas.hormozgan;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
@@ -8,5 +10,11 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(CityWidgetPlugin.class);
         super.onCreate(savedInstanceState);
+        Intent serviceIntent = new Intent(this, WeatherNotificationService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent);
+        } else {
+            startService(serviceIntent);
+        }
     }
 }
